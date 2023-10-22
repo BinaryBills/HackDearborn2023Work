0ca65c687611d70a484fb030acc3b43683e8627b
Han Lee <lee46@mail.usf.edu>
2023-10-22 05:47:18 -0400

    s

--- pages/Login.py
+++ pages/Login.py
@@ -1,9 +1,9 @@
 import oracledb
 import getpass
 
-user = "hlee3@ufl.edu"
+user = "hlee3"
 dsn = "ocidbdemo_high"
-pw = getpass.getpass(f"Enter password for {user}: ")
+pw = "DlGkS!2#4%6&8(0"
 wallet_pw = "DlGkS!2#4%6&8(0"
 
 con = oracledb.connect(user=user, password=pw, dsn=dsn, config_dir=r"./Wallet_RT3XMTA51QNCGH1G/", wallet_password=wallet_pw)
