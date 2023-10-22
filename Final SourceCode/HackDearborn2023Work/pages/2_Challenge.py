e9f74a5c14021bee10dde59bfd557ff3a5715295
Luffy D <84648682+BinaryBills@users.noreply.github.com>
2023-10-22 09:45:07 -0400

    Push

--- Final SourceCode/HackDearborn2023Work/pages/2_Challenge.py
+++ Final SourceCode/HackDearborn2023Work/pages/2_Challenge.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
@@ -12,7 +11,29 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-@st.cache
+
+
+st.title("Data Analysis")
+data = st.file_uploader("xlsx file", type =['xlsx'] )
+
+
+if data is not None:
+    df = pd.read_excel(data)
+    st.write(df.head())
+
+    x = st.text_area("Enter question about data:")
+
+    if st.button("Submit Data Question!"):
+        if x:
+            with st.spinner("Awaiting Response..."):
+              llm = ChatOpenAI(model_name='gpt-4', temperature=0.0)
+              agent = create_pandas_dataframe_agent(llm, df, verbose=True)
+              st.write(agent.run(x))
+        else:
+            st.warning("Insert prompt")
+
+load_dotenv()
+@st.cache_data
 def load_data():
     data = pd.read_excel('Yazaki.xlsx', engine='openpyxl')
     data['Capacity'].replace('NULL', 0, inplace=True)
@@ -84,9 +105,9 @@ def main():
     else:
         st.plotly_chart(fig, theme=None)
 
-    prompt = st.text_area("Enter question about data:")
+    prompt = st.text_area("Enter question about heatmap!:")
 
-    if st.button("Submit"):
+    if st.button("Submit Heatmap Question"):
         if prompt:
             with st.spinner("Awaiting Responsex..."):
               llm = ChatOpenAI(model_name='gpt-4', temperature=0.0)
@@ -97,78 +118,19 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-import pandas as pd
-import plotly.graph_objects as go
-
-def get_stock_status(capacity, usage):
-    if usage > capacity:
-        return "OS"
-    elif capacity > usage:
-        return "IS"
-    else:
-        return "NA"
-
-# Load the data
-data = pd.read_excel('Yazaki.xlsx', engine='openpyxl')
-data['Capacity'].replace('NULL', 0, inplace=True)
-data['Usage'].replace('NULL', 0, inplace=True)
-
-# Prompt the user for country input
-country_input = input("Please enter a country name (or press enter to skip): ")
-
-# Filter the data based on the user's input
-if country_input and country_input in data['CountryName'].unique():
-    data = data[data['CountryName'] == country_input]
-
-# Prompt the user for preferred logistics input
-logistics_input = input("Please enter a preferred logistics (or press enter to skip): ")
-# Filter the data based on the user's logistics input
-if logistics_input:
-    if logistics_input == "road":
-        data = data[data['PreferredLogistics'].isin(["road", "road/plane"])]
-    elif logistics_input == "plane":
-        data = data[data['PreferredLogistics'].isin(["plane", "road/plane"])]
-    elif logistics_input in data['PreferredLogistics'].unique():
-        data = data[data['PreferredLogistics'] == logistics_input]
 
 
-# Calculate the difference
-data['Difference'] = data['Capacity'] - data['Usage']
+with open("styles.css") as f:
+    css_file = f.read()
 
-# Sort the data based on the numeric portion of 'PlantName'
-data['PlantNumber'] = data['PlantName'].str.extract('(\d+)').astype(int)
-data = data.sort_values('PlantNumber')
+with open("home.html") as p:
+    home_html = p.read()
 
-# Pivot the table to get the data in matrix form
-pivot_data = data.pivot_table(index='PlantName', values=['Difference'], aggfunc='sum').T
+with open("footer.html") as p:
+    footer_html = p.read()
 
-# Create a heatmap
-fig = go.Figure(data=go.Heatmap(
-    z=pivot_data.values,
-    x=pivot_data.columns,
-    y=pivot_data.index,
-    colorscale='Viridis',
-    hoverongaps=False,
-    hoverinfo='z',
-    showscale=True))
+st.markdown(f"""{footer_html}""", unsafe_allow_html=True)
 
-# Add annotations based on the stock status
-for i in range(len(pivot_data.columns)):
-    capacity = data[data['PlantName'] == pivot_data.columns[i]]['Capacity'].sum()
-    usage = data[data['PlantName'] == pivot_data.columns[i]]['Usage'].sum()
-    status = get_stock_status(capacity, usage)
-    
-    fig.add_annotation(go.layout.Annotation(text=status,
-                                            x=pivot_data.columns[i],
-                                            y=pivot_data.index[0],
-                                            ax=0, ay=0,
-                                            font=dict(color='white' if abs(pivot_data.values[0][i]) > (0.5 * abs(pivot_data.values).max()) else 'black',
-                                                      size=10)))
+st.markdown(f"""<style>{css_file}</style>""", unsafe_allow_html=True)
 
-fig.update_layout(title='Heatmap Plant Availability',
-                  xaxis_title='Plant Name',
-                  yaxis_title='Available Capacity')
 
-fig.show()
->>>>>>> 515b0a4789e50a5a0ced66c59e335d88289f7a1a
