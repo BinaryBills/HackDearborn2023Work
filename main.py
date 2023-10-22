5a22e74b071efd2dfe5e06c821fc97f392ecb47c
Han Lee <lee46@mail.usf.edu>
2023-10-22 02:19:53 -0400

    sdf

--- main.py
+++ main.py
@@ -33,3 +33,19 @@ if data is not None:
         else:
             st.warning("Insert prompt")
 
+with open("styles.css") as f:
+    css_file = f.read()
+
+with open("home.html") as p:
+    home_html = p.read()
+
+
+
+with open("footer.html") as p:
+    footer_html = p.read()
+
+st.markdown(f"""{home_html}""", unsafe_allow_html=True)
+
+st.markdown(f"""{footer_html}""", unsafe_allow_html=True)
+
+st.markdown(f"""<style>{css_file}</style>""", unsafe_allow_html=True)
