93798b3b5ae3a78999d2692249d047b7ba4e8e57
Han Lee <lee46@mail.usf.edu>
2023-10-22 07:35:23 -0400

    s

--- db.py
+++ db.py
@@ -8,9 +8,9 @@ dsn = "f1egppo9qb4yi0kx_medium"
 pw = "DlGkS!2#4%6&8(0"
 wallet_pw = "DlGkS!2#4%6&8(0"
 
-con = oracledb.connect(user=user, password=pw, dsn=dsn, config_dir=r"./Wallet_F1EGPPO9QB4YI0KX/", wallet_password=wallet_pw)
+connection = oracledb.connect(user=user, password=pw, dsn=dsn, config_dir=r"./Wallet_F1EGPPO9QB4YI0KX/", wallet_password=wallet_pw)
 
-print("Database version:", con.version)
+print("Database version:", connection.version)
 
 
 
