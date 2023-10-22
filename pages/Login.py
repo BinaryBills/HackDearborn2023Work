20925a83fdee4d3b9a86724aff206c3206579a1d
Han Lee <lee46@mail.usf.edu>
2023-10-22 05:48:32 -0400

    x

--- pages/Login.py
+++ pages/Login.py
@@ -6,6 +6,6 @@ dsn = "ocidbdemo_high"
 pw = "DlGkS!2#4%6&8(0"
 wallet_pw = "DlGkS!2#4%6&8(0"
 
-con = oracledb.connect(user=user, password=pw, dsn=dsn, config_dir=r"./Wallet_RT3XMTA51QNCGH1G/", wallet_password=wallet_pw)
+con = oracledb.connect(user=user, password=pw, dsn=dsn, config_dir=r"./Wallet_F1EGPPO9QB4YI0KX/", wallet_password=wallet_pw)
 
 print("Database version:", con.version)
