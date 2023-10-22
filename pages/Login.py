226817fb9d298eea7902c27afda280e7f1415caa
Han Lee <lee46@mail.usf.edu>
2023-10-22 04:50:12 -0400

    s

--- pages/Login.py
+++ pages/Login.py
@@ -6,6 +6,6 @@ dsn = "ocidbdemo_high"
 pw = getpass.getpass(f"Enter password for {user}: ")
 wallet_pw = getpass.getpass("Enter wallet password for the database: ")
 
-con = oracledb.connect(user=user, password=pw, dsn=dsn, config_dir=r"C:\Users\18135\Downloads\Wallet_RT3XMTA51QNCGH1G", wallet_location=r"C:\Users\18135\Downloads\Wallet_RT3XMTA51QNCGH1G", wallet_password=wallet_pw)
+con = oracledb.connect(user=user, password=pw, dsn=dsn, config_dir=r"./Wallet_RT3XMTA51QNCGH1G/", wallet_password=wallet_pw)
 
 print("Database version:", con.version)
