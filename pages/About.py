41c52215d8ac95d7959abd80315fc2ab0ecc5183
Luffy D <84648682+BinaryBills@users.noreply.github.com>
2023-10-22 08:33:45 -0400

    Update

---
+++ pages/About.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+with open("styles.css") as f:
+    css_file = f.read()
+
+with open("about.html") as p:
+    home_html = p.read()
+
+with open("footer.html") as p:
+    footer_html = p.read()
+
+st.markdown(f"""{footer_html}""", unsafe_allow_html=True)
+
+st.markdown(f"""<style>{css_file}</style>""", unsafe_allow_html=True)
